0feff83458c068956bad92ed211fa396470ed34c
passenger37 <anandsingh8448535943@gmail.com>
2026-07-24 20:54:25 +0530

    feat(users): implement get user by id endpoint

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,20 +1,59 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  UseGuards,
+} from '@nestjs/common';
+
+import {
+  ApiBearerAuth,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
 import { JwtAuthGuard } from '../../../common/guards/jwt-auth.guard';
-import { ApiBearerAuth } from '@nestjs/swagger';
-
 import { CurrentUser } from '../../../common/decorators/current-user.decorator';
-import { UserMapper } from '../mappers/user.mapper';
 
+import { UsersService } from '../services/users.service';
+import { UserResponseDto } from '../responses';
+
+@ApiTags('Users')
 @Controller({
   path: 'users',
   version: '1',
 })
 export class UsersController {
-  @ApiBearerAuth('JWT')
+  constructor(private readonly usersService: UsersService) {}
+
   @Get('me')
   @UseGuards(JwtAuthGuard)
-  me(@CurrentUser() user: any) {
-    return UserMapper.toCurrentUser(user);
+  @ApiBearerAuth('JWT')
+  async getCurrentUser(@CurrentUser() user: { id: string }) {
+    return this.usersService.getCurrentUser(user.id);
+  }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Get user by ID',
+  })
+  @ApiOkResponse({
+    type: UserResponseDto,
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found.',
+  })
+  async getUserById(
+    @Param(
+      'id',
+      new ParseUUIDPipe({
+        version: '4',
+      }),
+    )
+    id: string,
+  ) {
+    return this.usersService.getUserById(id);
   }
 }
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
 import { UsersRepository } from './repositories/users.repository';
+import { UsersDomainService } from './domain/services/users-domain.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository],
+  providers: [UsersService, UsersRepository, UsersDomainService],
   exports: [UsersService, UsersRepository],
 })
 export class UsersModule {}
