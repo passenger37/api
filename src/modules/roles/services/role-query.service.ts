ee440dbdc5e2ea076b50f6bec42ecbd1fe7d21b8
passenger37 <anandsingh8448535943@gmail.com>
2026-07-31 11:32:03 +0530

    feat(roles): implement role query service

--- src/modules/roles/services/role-query.service.ts
+++ src/modules/roles/services/role-query.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { RoleMapper } from '../mappers';
+
+import { RolesRepository } from '../repositories/roles.repository';
+
+import { QueryRolesDto } from '../dto';
+
+import { RoleResponseDto } from '../responses';
 
 @Injectable()
-export class RoleQueryService {}
+export class RoleQueryService {
+  constructor(private readonly rolesRepository: RolesRepository) {}
+
+  // =====================================================
+  // Get Role By Id
+  // =====================================================
+
+  async getRoleById(id: string): Promise<RoleResponseDto> {
+    const role = await this.rolesRepository.findById(id);
+
+    if (!role) {
+      throw new NotFoundException('Role not found.');
+    }
+
+    return RoleMapper.toResponse(role);
+  }
+
+  // =====================================================
+  // Get Role By Name
+  // =====================================================
+
+  async getRoleByName(name: string): Promise<RoleResponseDto> {
+    const role = await this.rolesRepository.findByName(name);
+
+    if (!role) {
+      throw new NotFoundException('Role not found.');
+    }
+
+    return RoleMapper.toResponse(role);
+  }
+
+  // =====================================================
+  // Get All Roles
+  // =====================================================
+
+  async getRoles(query: QueryRolesDto): Promise<RoleResponseDto[]> {
+    // Pagination, search and sorting
+    // will be implemented in a later lecture.
+
+    const roles = await this.rolesRepository.findMany();
+
+    return RoleMapper.toResponseList(roles);
+  }
+}
