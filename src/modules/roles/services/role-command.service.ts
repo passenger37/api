f4c909d526ce870085d269665d7dde1df87c37d6
passenger37 <anandsingh8448535943@gmail.com>
2026-07-31 11:43:57 +0530

    feat(roles): implement role command service

--- src/modules/roles/services/role-command.service.ts
+++ src/modules/roles/services/role-command.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
 
+import { CreateRoleDto, UpdateRoleDto } from '../dto';
+
+import { RoleResponseDto } from '../responses';
+
+import { RoleMapper } from '../mappers';
+
+import { RoleDomainService } from './role-domain.service';
+
 @Injectable()
-export class RoleCommandService {}
+export class RoleCommandService {
+  constructor(private readonly roleDomainService: RoleDomainService) {}
+
+  // =====================================================
+  // Create
+  // =====================================================
+
+  async create(dto: CreateRoleDto): Promise<RoleResponseDto> {
+    const role = await this.roleDomainService.create(dto);
+
+    return RoleMapper.toResponse(role);
+  }
+
+  // =====================================================
+  // Update
+  // =====================================================
+
+  async update(id: string, dto: UpdateRoleDto): Promise<RoleResponseDto> {
+    const role = await this.roleDomainService.update(id, dto);
+
+    return RoleMapper.toResponse(role);
+  }
+
+  // =====================================================
+  // Delete
+  // =====================================================
+
+  async delete(id: string): Promise<RoleResponseDto> {
+    const role = await this.roleDomainService.delete(id);
+
+    return RoleMapper.toResponse(role);
+  }
+}
