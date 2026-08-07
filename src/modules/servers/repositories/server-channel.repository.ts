bf8ed38a74611a7a9a69081ca98f0488a72f71af
passenger37 <anandsingh8448535943@gmail.com>
2026-08-07 09:59:42 +0530

    feat(channels): implement server channel repository

--- src/modules/servers/repositories/server-channel.repository.ts
+++ src/modules/servers/repositories/server-channel.repository.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+
+import { Prisma, ServerChannel } from '@prisma/client';
 
 import { PrismaService } from '../../../core/database/prisma.service';
 
@@ -9,17 +10,96 @@ export class ServerChannelRepository {
 
   async create(
     data: Prisma.ServerChannelCreateInput,
-    tx: Prisma.TransactionClient,
-  ) {
-    return tx.serverChannel.create({
+    prisma: Prisma.TransactionClient | PrismaService = this.prisma,
+  ): Promise<ServerChannel> {
+    return prisma.serverChannel.create({
       data,
     });
   }
 
-  async findById(id: string) {
+  async update(
+    id: string,
+    data: Prisma.ServerChannelUpdateInput,
+    prisma: Prisma.TransactionClient | PrismaService = this.prisma,
+  ): Promise<ServerChannel> {
+    return prisma.serverChannel.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+
+  async delete(
+    id: string,
+    prisma: Prisma.TransactionClient | PrismaService = this.prisma,
+  ): Promise<ServerChannel> {
+    return prisma.serverChannel.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async findById(id: string): Promise<ServerChannel | null> {
     return this.prisma.serverChannel.findUnique({
       where: {
         id,
+      },
+    });
+  }
+
+  async findByName(
+    serverId: string,
+    name: string,
+  ): Promise<ServerChannel | null> {
+    return this.prisma.serverChannel.findFirst({
+      where: {
+        serverId,
+        name,
+      },
+    });
+  }
+
+  async findMany(serverId: string): Promise<ServerChannel[]> {
+    return this.prisma.serverChannel.findMany({
+      where: {
+        serverId,
+      },
+      orderBy: [
+        {
+          position: 'asc',
+        },
+      ],
+    });
+  }
+
+  async getHighestPosition(serverId: string): Promise<number> {
+    const channel = await this.prisma.serverChannel.findFirst({
+      where: {
+        serverId,
+      },
+      orderBy: {
+        position: 'desc',
+      },
+    });
+
+    return channel?.position ?? 0;
+  }
+
+  async search(serverId: string, query: string): Promise<ServerChannel[]> {
+    return this.prisma.serverChannel.findMany({
+      where: {
+        serverId,
+
+        name: {
+          contains: query,
+          mode: 'insensitive',
+        },
+      },
+
+      orderBy: {
+        position: 'asc',
       },
     });
   }
